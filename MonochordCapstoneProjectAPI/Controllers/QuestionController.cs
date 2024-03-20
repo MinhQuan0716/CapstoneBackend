4a301cbfe33dda44ab1becdf0b3e9170c4a8cb12
MinhQuan778910 <minhquan2002232910@gmail.com>
2024-03-20 16:55:48 +0700

    Save change

---
+++ MonochordCapstoneProjectAPI/Controllers/QuestionController.cs
@@ -0,0 +1,28 @@
+﻿using Application.InterfaceService;
+using Application.ViewModel.ResponeModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MonochordCapstoneProjectAPI.Controllers
+{
+  
+    public class QuestionController : MainController
+    {
+        private readonly IQuestionService _questionService;
+        public QuestionController(IQuestionService questionService)
+        {
+            _questionService = questionService;
+        }
+        /// <summary>
+        /// Get question in quiz
+        /// </summary>
+        /// <param name="id">The quiz Id</param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public async Task<Respone> GetAllQuestionByQuizId (Guid id )
+        {
+            Respone respone = await _questionService.GetQuestionByQuizIdAsync(id);
+            return respone;
+        }
+    }
+}
