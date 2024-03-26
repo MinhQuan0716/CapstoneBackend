0c352198b8cfbdc322cb4be776efebbb07a75fab
MinhQuan778910 <minhquan2002232910@gmail.com>
2024-03-26 22:28:33 +0700

    Modify random string function for Forgotpassword

--- Application/Util/StringUtil.cs
+++ Application/Util/StringUtil.cs
@@ -17,7 +17,7 @@ namespace Application.Util
 
         public static string RandomString(int length)
         {
-            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            const string chars = "0123456789";
             return new string(Enumerable.Repeat(chars, length)
                 .Select(s => s[random.Next(s.Length)]).ToArray());
         }
--- Domain/Entities/Payment.cs
+++ Domain/Entities/Payment.cs
@@ -11,7 +11,7 @@ namespace Domain.Entities
         public Guid SubcriptionId { get; set; }
         public Subcription Subcription { get; set; }
         public Guid AccountId { get; set; }
-        public Account Account { get; set; } 
+        public Account Account { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public string Status { get; set; }
