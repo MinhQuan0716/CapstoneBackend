a4b4a907d46a57c18484fbf183ce1a6c3cbc35d4
Minhtda <113592001+Minhtda@users.noreply.github.com>
2024-04-02 20:04:18 +0700

    fix login google

--- Application/Services/AccountService.cs
+++ Application/Services/AccountService.cs
@@ -204,7 +204,6 @@ namespace Application.Services
             try
             {
                 var payload = await GoogleJsonWebSignature.ValidateAsync(token);
-                string userId = payload.Subject;
                 string email = payload.Email;
                 string firstName = payload.GivenName;
                 string lastName = payload.FamilyName;
@@ -216,6 +215,7 @@ namespace Application.Services
                     newAcc.Email = email;
                     newAcc.RoleId = 3;
                     newAcc.IsDelete = false;
+                    newAcc.UserName = firstName + lastName;
                     newAcc.FirstName = firstName;
                     newAcc.LastName = lastName;
                     newAcc.ImageUrl = pictureUrl;
