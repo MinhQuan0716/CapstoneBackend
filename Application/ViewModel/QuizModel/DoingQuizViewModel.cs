817f2abc5398465c146cbac902916cb84a4a83b7
MinhQuan778910 <minhquan2002232910@gmail.com>
2024-04-02 20:35:37 +0700

    Add UserQuizAttemptRepository

---
+++ Application/ViewModel/QuizModel/DoingQuizViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.ViewModel.QuizModel
+{
+    public  class DoingQuizViewModel
+    {
+        public Guid QuestionId { get; set; }
+        public Guid ChoiceId { get; set; }
+    }
+}
---
+++ Infrastructure/Repository/UserQuizAttemptRepository.cs
@@ -0,0 +1,21 @@
+﻿using Application.InterfaceRepository;
+using Application.InterfaceService;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repository
+{
+    public class UserQuizAttemptRepository : GenericRepository<UserQuizAttempt>, IUserQuizAttemptRepository
+    {
+        private readonly AppDbContext _appDbContext;
+        public UserQuizAttemptRepository(AppDbContext appDbContext, IClaimService claimService) : base(appDbContext, claimService)
+        {
+            _appDbContext = appDbContext;
+        }
+        
+    }
+}
